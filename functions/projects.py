09ad1f308914976f79e42a455a991a86f27d6277
IsuruG99 <isuru9917@gmail.com>
2024-02-18 15:27:35 +0530

    Add Project Function Complete.

--- functions/projects.py
+++ functions/projects.py
@@ -1,10 +1,11 @@
 import database
 
 
-def add_project(name, description, start_date, end_date, client_id, budget, status):
+def add_project(name, description, start_date, end_date, client_name, budget, status):
     # Get a reference to the 'projects' branch in the Firebase Realtime Database
     ref = database.get_ref('projects')
 
+    print(name, description, start_date, end_date, client_name, budget, status)
     if ref is not None:
         # Generate a new unique key for the project
         new_project_ref = ref.push()
@@ -15,7 +16,7 @@ def add_project(name, description, start_date, end_date, client_id, budget, stat
             'description': description,
             'start_date': start_date,
             'end_date': end_date,
-            'client_id': client_id,
+            'client_name': client_name,
             'budget': budget,
             'status': status
         })
@@ -26,13 +27,13 @@ def add_project(name, description, start_date, end_date, client_id, budget, stat
 
 
 # Test adding a new project record
-if __name__ == "__main__":
-    add_project(
-        name="New Project",
-        description="This is a new project",
-        start_date="2024-01-01",
-        end_date="2024-12-31",
-        client_id="client1",
-        budget=1000000,
-        status="in progress"
-    )
+# if __name__ == "__main__":
+# add_project(
+#    name="New Project",
+#    description="This is a new project",
+#    start_date="2024-01-01",
+#    end_date="2024-12-31",
+#    client_name="client1",
+#    budget=1000000,
+#     status="in progress"
+# )
--- utils.py
+++ utils.py
@@ -1,8 +1,22 @@
-# validate string function that checks if a string is null or not, may take any amount of inputs
+import datetime
 
+
+# Check for Null Inputs
 def validate_string(*args):
     for arg in args:
         if arg is None or arg == '':
+            return False
+    return True
+
+
+# Validate Date Strings
+def validate_date(*args):
+    for arg in args:
+        if arg is None or arg == '':
+            return False
+        try:
+            datetime.datetime.strptime(arg, '%Y-%m-%d')
+        except ValueError:
             return False
     return True
 
